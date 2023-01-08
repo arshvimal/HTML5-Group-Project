6f5ff14d67cd14dcbe3d359221bc56f8db0049cf
Arsh Vimal <arsh11vimal@gmail.com>
2023-01-08 19:50:20 +0530

    Updates (Read Description)
    Added upscaled player assets and modified the code to have proper ground and player animation and also that the player is properly placed on the ground

--- game.js
+++ game.js
@@ -12,25 +12,38 @@ const obstacles = [];
 
 const groundImage = new Image();
 groundImage.src = './assets/ground.png';
+const spriteSheet = new Image();
+spriteSheet.src = './assets/player.png';
+
+let frameCounter = 0;
+
 
 const ground = {
   x: 0,
   y: (canvas.height-100), // position the ground at the bottom of the canvas
   width: 100,
   height: 100,
-  speed: 5
 };
 
 // Set up player
+
+let upscale = 4;
+
 const player = {
-  x: 50,
-  y: canvas.height / 2,
-  width: 20,
-  height: 20,
+  width: 48*upscale,
+  height: 48*upscale,
+  PerRow: 6,
+  rows: 10,
+  total: 6,
+  current: 0,
+  x: 0,
+  y: 0,
   speed: 2,
-  jumpForce: 100,
-  jumping: false
 };
+player.y = (canvas.height-100-(48*upscale))
+
+
+let frameDelay = 24/player.speed;
 
 function update() {
     // Check if player is playing
@@ -39,6 +52,12 @@ function update() {
     }
     // Update ground position
   ground.x -= player.speed;
+
+  frameCounter++;
+  if (frameCounter >= frameDelay) {
+    player.current = (player.current + 1) % player.total;
+    frameCounter = 0;
+  }
 
   // Check if ground is off screen
     if (ground.x + ground.width < 0) {
@@ -61,13 +80,19 @@ function update() {
       ctx.drawImage(groundImage, ground.x + groundend, ground.y, ground.width, ground.height);
       groundend = groundend + ground.width;
     }
-    
+    col = player.current % player.PerRow;
+    row = 4; //running
+    ctx.drawImage(spriteSheet, col*player.width, row*player.height, player.width, player.height, player.x, player.y + (8*upscale), player.height, player.width)
     
   
   }
   
   // Set up game loop
   function loop() {
+    console.log("func loop is running")
+    if(!isPlaying){
+      return;
+    }
     update();
     draw();
     requestAnimationFrame(loop);
@@ -75,16 +100,37 @@ function update() {
   
   // Start game
   function startGame() {
+    console.log("func start is running")
     // Reset game variables
     isPlaying = true;
     score = 0;
-    player.x = 50;
-    player.y = canvas.height / 2;
-    obstacles.length = 0;
   
     // Start game loop
     loop();
   }
+  function stopGame() {
+    console.log("func stop is running")
+    isPlaying = false;
+    score = 0;
+    
+  }
+
+  function restartGame() {
+    console.log("func restart is running")
+    if (isPlaying){
+      stopGame();
+    }
+    else{
+      startGame();
+    }
+  }
 // Add event listener to start button
 const startButton = document.getElementById('startButton');
-startButton.addEventListener('click', startGame);
+startButton.addEventListener('click', restartGame);
+startButton.addEventListener('click', function() {
+  if (this.innerHTML === 'Start Game') {
+    this.innerHTML = 'Stop Game';
+  } else {
+    this.innerHTML = 'Start Game';
+  }
+});
